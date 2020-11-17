882a1d0d6685f48709c4f35594bcbf29b0c42f48
Vipul dev <vipul@Vipuls-MacBook-Air.local>
2020-11-17 23:27:45 +0530

    Modified ExpenseListitem

--- src/components/ExpenseItemList.js
+++ src/components/ExpenseItemList.js
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import moment from 'moment';
 import{Link } from 'react-router-dom';
-
+import numeral from 'numeral';
 
 const ExpenseListItem =({description,id , createdAt , amount})=>(
 <div>  
 <Link to ={`/edit/${id}`}>
  <h1>{description} </h1>
  </Link>
-<p> {amount } --- {createdAt}</p>
+<p> 
+{numeral(amount).format('0,0.00')}
+
+---
+
+
+{moment(createdAt).format('MMMM Do, YYYY')}
+
+</p>
 
 </div>
 );
